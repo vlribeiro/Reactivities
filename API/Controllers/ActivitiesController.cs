11bf610ff2adf74528b690ce6aa68c3b1fbeb2ed
Vinícius Ribeiro <vl.ribeiro3@gmail.com>
2025-08-21 16:53:24 -0300

    New controller methods using MediatR and Automapper

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,26 +1,41 @@
+using Application.Activities.Commands;
+using Application.Activities.Queries;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers;
 
-public class ActivitiesController(AppDbContext context) : BaseApiController
+public class ActivitiesController : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await context.Activities.ToListAsync();
+        return await this.Mediator.Send(new GetActivityList.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        var activity = await context.Activities.FindAsync(id);
-
-        if (activity == null) return NotFound();
-
-        return activity;
+        return await this.Mediator.Send(new GetActivityDetails.Query { Id = id });
     }
 
+    [HttpPost]
+    public async Task<ActionResult<string>> CreateActivity(Activity activity)
+    {
+        return await this.Mediator.Send(new CreateActivity.Command { Activity = activity });
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> EditActivity(Activity activity)
+    {
+        await this.Mediator.Send(new EditActivity.Command { Activity = activity });
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteActivity(string Id)
+    {
+        await this.Mediator.Send(new DeleteActivity.Command { Id = Id });
+        return Ok();
+    }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -10,6 +12,11 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.AddProfile(typeof(MappingProfiles));
+});
 
 var app = builder.Build();
 
