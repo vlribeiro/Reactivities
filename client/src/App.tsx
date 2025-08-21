0f464fb0ee9fc7204952b9c125244b61ba49c97d
Vinícius Ribeiro <vl.ribeiro3@gmail.com>
2025-08-21 09:49:02 -0300

    Initial client project, using React and Vite

---
+++ client/src/App.tsx
@@ -0,0 +1,27 @@
+import { Typography, List, ListItem, ListItemText } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get<Activity[]>('https://localhost:5001/api/Activities')
+      .then(response => setActivities(response.data))
+  }, [])
+
+  return (
+    <>
+      <Typography variant="h1">Reactivities</Typography>
+      <List>
+        {activities.map(activity => (
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  )
+}
+
+export default App
